339094d2d63cdc526b19dd122a1556c828c02fc4
Larsen, James B <JamesB444@msn.com>
2018-09-16 23:08:56 -0600

    "First commit"

---
+++ EuropanIndependence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EuropanIndependence
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new EuropanIndependence())
+                game.Run();
+        }
+    }
+}
